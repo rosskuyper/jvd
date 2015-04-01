485db6f4f1d58b9b12d6593efda865406bd2fce9
Ross Kuyper <rosskuyper@gmail.com>
2015-04-01 20:20:40 +0200

    En copy

--- app/Views/sections/quals.php
+++ app/Views/sections/quals.php
@@ -1,26 +1,41 @@
+<?php
+
+// External links
+echo $app->view->fetch('partials/dual-columns.php', [
+	'titleName' => 'quals.quals.title',
+	'itemName'  => 'quals.quals.content',
+]);
+
+?>
+
 <section class="clearfix">
 	<div class="left">
-		<h5><?= $lang->get('quals.quals.title'); ?></h5>
-		<p><?= $lang->get('quals.quals.content'); ?></p>
-	</div>
-	<div class="right">
 		<h5><?= $lang->get('quals.creds.title'); ?></h5>
 		<p><?= $lang->get('quals.creds.content'); ?></p>
+	</div>
+	<div class="right">
+		<?php
+		// External links
+		echo $app->view->fetch('partials/link-list.php', [
+			'titleName' => 'quals.links.title',
+			'itemName'  => 'quals.links.items',
+		]);
+		?>
 	</div>
 </section>
 
 <?php
 
-// External links
-echo $app->view->fetch('partials/link-list.php', [
-	'titleName' => 'quals.links.title',
-	'itemName'  => 'quals.links.items',
-]);
-
 // Clients
 echo $app->view->fetch('partials/dual-columns.php', [
 	'titleName' => 'quals.clients.title',
 	'itemName'  => 'quals.clients.content',
+]);
+
+// Samples
+echo $app->view->fetch('partials/link-list.php', [
+	'titleName' => 'quals.articles.title',
+	'itemName'  => 'quals.articles',
 ]);
 
 // Samples
